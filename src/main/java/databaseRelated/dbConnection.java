c7ce1d4f1f4f4143c3df1eb9e10e675e5b4d0cb5
Manoj Kumar <mady.palawat@gmail.com>
2022-02-09 23:24:47 +0530

    new1

--- src/main/java/databaseRelated/dbConnection.java
+++ src/main/java/databaseRelated/dbConnection.java
@@ -8,7 +8,7 @@ import  java.sql.ResultSet;
 import  java.sql.DriverManager;		
 import  java.sql.SQLException;		
 public class  dbConnection {				
-	//ework1
+	//new
 
 /*	public static void sendPingRequest(String ipAddress)
             throws UnknownHostException, IOException
